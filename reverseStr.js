305877b3134364295213515de05044fb8595c1f3
aysha-codes <aysha.springston@gmail.com>
2022-03-21 13:19:05 -0500

    Add solution

--- reverseStr.js
+++ reverseStr.js
@@ -4,5 +4,9 @@
 // 'word'   =>  'drow'
 
 function solution(str){
-  
-}
+    let revStr = '';
+    for (i = str.length-1; i >= 0; i--) {
+      revStr = revStr + str[i]
+    }
+    return revStr
+  }
