8a7c39481b673ce7fd870df932e0e11ca851274b
aysha-codes <aysha.springston@gmail.com>
2022-03-22 13:22:30 -0500

    Add solution

--- posSum.js
+++ posSum.js
@@ -5,5 +5,7 @@
 // Note: if there is nothing to sum, the sum is default to 0.
 
 function positiveSum(arr) {
-  
-}
+    summedArr = 0;
+    arr.forEach(e => Math.sign(e) === 1 ? summedArr += e : null )
+    return summedArr
+  }
