755b33df3c3adc0632250f6a6d3b875a1b2d6f1d
aysha-codes <aysha.springston@gmail.com>
2022-03-27 13:46:55 -0500

    Add solution

--- summation.js
+++ summation.js
@@ -9,8 +9,8 @@
 // 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
 
 var summation = function (num) {
-    let sum = 1;
-    for(let i = 1; i < num; i++){
+    let sum = 0;
+    for(let i = 1; i <= num; i++){
       sum += i;
     }
     return sum;
