1f7c74e590b0883683dae690c9fd3d9f0269e73e
aysha-codes <aysha.springston@gmail.com>
2022-04-14 22:10:03 -0500

    Attempted Solution

--- posCountNegSum.js
+++ posCountNegSum.js
@@ -8,5 +8,29 @@
 // For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
 
 function countPositivesSumNegatives(input) {
-    return ?;
-}
+    const posItems = []
+    const negItems = []
+    input.forEach(i => {
+      if (Math.sign(i) === 1){
+        posItems.push(i);
+      } else if (Math.sign(i) === -1){
+        negItems.push(i);
+      }
+    })
+  
+    let negSum = negItems.reduce(
+      ( previousValue, currentValue ) => previousValue + currentValue,
+      0
+      )
+    const finalArr = [posItems.length, negSum]
+  
+    return finalArr;
+  }
+
+// Pseudo Code
+// Iterate over the array, make two new arrays
+// > One with all the positive numbers
+// > one with all the negative numbers
+// - For the positive array, get length; will be count
+// - for the negative array, sum elements
+// - Return both in a new array
