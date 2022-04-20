981c07f72a167b2ee229c65de25e89791d90a0d7
aysha-codes <aysha.springston@gmail.com>
2022-04-19 23:09:35 -0500

    Add solution

--- reversedSeq.js
+++ reversedSeq.js
@@ -3,5 +3,11 @@
 // Example : n=5 --> [5,4,3,2,1]
 
 const reverseSeq = n => {
-    return [];
-  };
+    const arr = [];
+    if (n > 0) {
+      for (let i = n; i > 0; i--){
+        arr.push(i);
+      }
+      return arr
+    }
+  }
