0f4c77a692b6b491374770f771f0a48fb15359ca
aysha-codes <aysha.springston@gmail.com>
2022-04-20 14:27:07 -0500

    Add solution

--- DNAtoRNA.js
+++ DNAtoRNA.js
@@ -10,5 +10,5 @@
 
 // The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
 function DNAtoRNA(dna) {
-    // create a function which returns an RNA sequence from the given DNA sequence
+    return dna.replace(/[T]/gm, "U")
   }
