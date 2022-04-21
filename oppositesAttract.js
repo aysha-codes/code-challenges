6191bf76c5e3a2e08bbe6db10e90616870c260b7
aysha-codes <aysha.springston@gmail.com>
2022-04-21 14:53:19 -0500

    Attempted solution

--- oppositesAttract.js
+++ oppositesAttract.js
@@ -2,5 +2,11 @@
 
 // Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
 function lovefunc(flower1, flower2){
-    // moment of truth
+    let one = (flower1 % 2 === 0)
+    let two = (flower2 % 2 === 0)
+    if (one && two){
+      return true
+    } else {
+      return false
+    }
   }
