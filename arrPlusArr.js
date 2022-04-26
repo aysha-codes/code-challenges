2b46da0236ac7ff044777ed43cb007a600bde62f
Aysha S <aysha.springston@gmail.com>
2022-04-26 09:51:23 -0500

    Refactor solution

--- arrPlusArr.js
+++ arrPlusArr.js
@@ -2,5 +2,5 @@
 
 // P.S. Each array includes only integer numbers. Output is a number too.
 function arrayPlusArray(arr1, arr2) {
-    return arr1.reduce((p,c) => p+c,0) + arr2.reduce((p,c) => p+c,0)
-  }
+    return arr1.concat(arr2).reduce((acc, cur) => acc + cur);
+  }  
