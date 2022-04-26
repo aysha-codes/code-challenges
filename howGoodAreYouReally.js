0809f8c9dd961902857122ca07b6ea567286cff0
Aysha S <aysha.springston@gmail.com>
2022-04-26 10:37:53 -0500

    Add solution

--- howGoodAreYouReally.js
+++ howGoodAreYouReally.js
@@ -8,5 +8,7 @@
 // Note:
 // Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!
 function betterThanAverage(classPoints, yourPoints) {
-    // Your code here
+    classPoints.push(yourPoints);
+    let avg = classPoints.reduce((a,b) => a+b) / classPoints.length
+    return (yourPoints > avg) ? true : false;
   }  
