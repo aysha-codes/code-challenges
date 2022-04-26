db03f19659058a67f70d8259d344944a6c65c324
Aysha S <aysha.springston@gmail.com>
2022-04-26 14:07:16 -0500

    Add solution

--- convertStrToArr.js
+++ convertStrToArr.js
@@ -5,7 +5,5 @@
 
 // * "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
 function stringToArray(string){
-
-	// code code code
-
-}
+    return string.split(' ');
+  }
