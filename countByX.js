a8d5db761bd2137cc68498fc3d3e0c6b524a3100
Aysha S <aysha.springston@gmail.com>
2022-04-27 14:23:19 -0500

    Add solution

--- countByX.js
+++ countByX.js
@@ -9,7 +9,11 @@
 // countBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
 // countBy(2,5) === [2,4,6,8,10]
 function countBy(x, n) {
-    let z = [];
-  
-    return z;
-  }
+  let z = [];
+  let y = Number(x*n);
+  let j = Number(x);
+    for(let i = j;i <= y;i+=j){
+      z.push(i);
+    }
+  return z;
+}
