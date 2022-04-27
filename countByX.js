ac806ac9db6650e52ce7457ad021617e38699dbf
Aysha S <aysha.springston@gmail.com>
2022-04-27 14:24:31 -0500

    Refactor solution

--- countByX.js
+++ countByX.js
@@ -9,11 +9,9 @@
 // countBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
 // countBy(2,5) === [2,4,6,8,10]
 function countBy(x, n) {
-  let z = [];
-  let y = Number(x*n);
-  let j = Number(x);
-    for(let i = j;i <= y;i+=j){
-      z.push(i);
-    }
+  var z = [];
+  for (i = 1; i <= n; i++) {
+      z.push(x * i);
+  }
   return z;
 }
