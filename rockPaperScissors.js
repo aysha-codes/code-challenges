f2f12eaa7158e2a825e4da2931fb9aa7de5f74c9
Aysha S <aysha.springston@gmail.com>
2022-04-28 10:01:02 -0500

    Add solution

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -7,4 +7,19 @@
 // rps('scissors','rock') // Player 2 won!
 // rps('paper','paper') // Draw!
 const rps = (p1, p2) => {
-};
+    if(p1 === p2) {
+      return 'Draw!';
+    }else if(
+      (p1 === 'rock' && p2 === 'scissors') ||
+      (p1 === 'scissors' && p2 === 'paper') ||
+      (p1 === 'paper' && p2 === 'rock')
+    ){
+      return "Player 1 won!";
+    }else if(
+      (p1 === 'scissors' && p2 === 'rock') ||
+      (p1 === 'paper' && p2 === 'scissors') ||
+      (p1 === 'rock' && p2 === 'paper')
+    ){
+      return "Player 2 won!";
+      }
+  };
