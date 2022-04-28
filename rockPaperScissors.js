8471f597dd3e7e96f89e9dd763b63626bd4c99fc
Aysha S <aysha.springston@gmail.com>
2022-04-28 14:18:02 -0500

    Refactor solution

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -7,19 +7,12 @@
 // rps('scissors','rock') // Player 2 won!
 // rps('paper','paper') // Draw!
 const rps = (p1, p2) => {
-    if(p1 === p2) {
-      return 'Draw!';
-    }else if(
-      (p1 === 'rock' && p2 === 'scissors') ||
-      (p1 === 'scissors' && p2 === 'paper') ||
-      (p1 === 'paper' && p2 === 'rock')
-    ){
-      return "Player 1 won!";
-    }else if(
-      (p1 === 'scissors' && p2 === 'rock') ||
-      (p1 === 'paper' && p2 === 'scissors') ||
-      (p1 === 'rock' && p2 === 'paper')
-    ){
-      return "Player 2 won!";
-      }
-  };
+  if (p1 === p2) return "Draw!";
+  var rules = {rock: "scissors", paper: "rock", scissors: "paper"}; // using object to match k/v pairs
+  if (p2 === rules[p1]) {
+    return "Player 1 won!";
+  }
+  else {
+    return "Player 2 won!";
+  }
+};
