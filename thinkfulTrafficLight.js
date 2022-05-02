34ee69864004286aee047b6e0af1163e9d6bbde5
Aysha S <aysha.springston@gmail.com>
2022-05-02 11:14:19 -0500

    Refactor solution

--- thinkfulTrafficLight.js
+++ thinkfulTrafficLight.js
@@ -3,16 +3,8 @@
 // Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
 
 // For example, update_light('green') should return 'yellow'.
-function updateLight(current) {
-    switch(current){
-        case 'green':
-          return 'yellow'
-          break;
-        case 'yellow':
-          return 'red'
-          break;
-        case 'red':
-          return 'green'
-          break;
-    }
-  }
+const updateLight = current => ({
+  green: 'yellow',
+  yellow: 'red',
+  red: 'green',
+})[current]
