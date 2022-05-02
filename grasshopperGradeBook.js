360b4966133c73ad970e3e671081803ecb0e5701
aysha-codes <aysha.springston@gmail.com>
2022-05-01 23:54:25 -0500

    Add solution

--- grasshopperGradeBook.js
+++ grasshopperGradeBook.js
@@ -10,5 +10,16 @@
 
 // Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
 function getGrade (s1, s2, s3) {
-    // Code here
+    let score = (s1 + s2 + s3) / 3
+    if (90 <= score){
+      return 'A';
+    } else if (80 <= score){
+      return 'B';
+    } else if (70 <= score){
+      return 'C';
+    } else if (60 <= score){
+      return 'D'
+    } else {
+      return 'F'
+    }
   }
