fd5354f0e701d92806b48caeb43faf3646777318
Aysha S <aysha.springston@gmail.com>
2022-05-05 15:29:18 -0500

    Refactor solution

--- twiceAsOld.js
+++ twiceAsOld.js
@@ -5,4 +5,5 @@
 // Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old).
 function twiceAsOld(dadYearsOld, sonYearsOld) {
     return Math.abs(dadYearsOld - 2 * sonYearsOld);
+    // multiplies son's age by 2, then subtracts it from dad's current age, which gives us the difference or how many years until the dad will be twice as old as the son
   }
