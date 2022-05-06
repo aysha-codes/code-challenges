c8688c5de9339a8b1a9377767ca0ef862a94fd9e
Aysha S <aysha.springston@gmail.com>
2022-05-06 15:38:44 -0500

    Refactor solution

--- switchItUp.js
+++ switchItUp.js
@@ -6,36 +6,17 @@
 
 // If your language supports it, try using a switch statement.
 function switchItUp(number){
-    switch(number){
-        case 0:
-          return "Zero";
-          break;
-        case 1:
-          return "One";
-          break;
-        case 2:
-          return "Two";
-          break;
-        case 3:
-          return "Three";
-          break;
-        case 4:
-          return "Four";
-          break;
-        case 5:
-          return "Five";
-          break;
-        case 6:
-          return "Six";
-          break;
-        case 7:
-          return "Seven";
-          break;
-        case 8:
-          return "Eight";
-          break;
-        case 9:
-          return "Nine";
-          break;
+    const nums = {
+      0: 'Zero',
+      1: 'One',
+      2: 'Two',
+      3: 'Three',
+      4: 'Four',
+      5: 'Five',
+      6: 'Six',
+      7: 'Seven',
+      8: 'Eight',
+      9: 'Nine'
     }
+    return nums[number];
   }
