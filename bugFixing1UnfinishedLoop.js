d04f779aaa6ad4ef604285f1a57a700e378f15c4
Aysha S <aysha.springston@gmail.com>
2022-05-09 10:56:01 -0500

    Refactor solution

--- bugFixing1UnfinishedLoop.js
+++ bugFixing1UnfinishedLoop.js
@@ -3,9 +3,8 @@
 function createArray(number){
     var newArray = [];
     
-    for(var counter = 1; counter <= number;){
+    for(var counter = 1; counter <= number; counter++){
       newArray.push(counter);
-      counter++
     }
     
     return newArray;
