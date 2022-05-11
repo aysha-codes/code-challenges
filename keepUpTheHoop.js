ed6d1014d574f3022ecd54a435bd14bd10cd15db
aysha-codes <aysha.springston@gmail.com>
2022-05-10 23:56:02 -0500

    Add solution

--- keepUpTheHoop.js
+++ keepUpTheHoop.js
@@ -5,5 +5,5 @@
 //     If Alex gets 10 or more hoops, return the string "Great, now move on to tricks".
 //     If he doesn't get 10 hoops, return the string "Keep at it until you get it".
 function hoopCount (n) {
-    //your code goes here    
-}
+    return (n >= 10) ? "Great, now move on to tricks" : "Keep at it until you get it";
+ }
