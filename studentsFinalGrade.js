4f8a58b6e3117f00fa77128af93b4aac32bebb9f
aysha-codes <aysha.springston@gmail.com>
2022-05-11 20:25:37 -0500

    Refactor solution

--- studentsFinalGrade.js
+++ studentsFinalGrade.js
@@ -24,13 +24,8 @@
 
 // *Use Comparison and Logical Operators.
 function finalGrade (exam, projects) {
-    if (exam > 90 || projects > 10){
-      return 100;
-    } else if (exam > 75 && projects > 4){
-      return 90;
-    } else if (exam > 50 && projects > 1){
-      return 75;
-    } else {
-      return 0;
-    }
-  }
+    if (exam > 90 || projects > 10) return 100;
+    if (exam > 75 && projects > 4) return 90;
+    if (exam > 50 && projects > 1) return 75;
+    return 0;
+}
