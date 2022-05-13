0e5824d74b0b3cebac645bb327320702e38d5964
aysha-codes <aysha.springston@gmail.com>
2022-05-12 20:36:31 -0500

    Add solution

--- welcome.js
+++ welcome.js
@@ -30,5 +30,27 @@
 // IP_ADDRESS_NOT_FOUND - ip address not in the database
 // IP_ADDRESS_REQUIRED - no ip address was supplied
 function greet(language) {
-    
+	const db = {
+    english: 'Welcome',
+    czech: 'Vitejte',
+    danish: 'Velkomst',
+    dutch: 'Welkom',
+    estonian: 'Tere tulemast',
+    finnish: 'Tervetuloa',
+    flemish: 'Welgekomen',
+    french: 'Bienvenue',
+    german: 'Willkommen',
+    irish: 'Failte',
+    italian: 'Benvenuto',
+    latvian: 'Gaidits',
+    lithuanian: 'Laukiamas',
+    polish: 'Witamy',
+    spanish: 'Bienvenido',
+    swedish: 'Valkommen',
+    welsh: 'Croeso',
+    IP_ADDRESS_INVALID: 'Welcome',
+    IP_ADDRESS_NOT_FOUND: 'Welcome',
+    IP_ADDRESS_REQUIRED: 'Welcome'
+  }
+  return db[language];
 }
