9877305302b242deae4cec32f914bab4f007b11c
aysha-codes <aysha.springston@gmail.com>
2022-05-23 18:50:49 -0500

    Add solution

--- iLoveYouALittleALotPassionatelyNotAtAll.js
+++ iLoveYouALittleALotPassionatelyNotAtAll.js
@@ -11,5 +11,14 @@
 
 // Your goal in this kata is to determine which phrase the girls would say for a flower of a given number of petals, where nb_petals > 0.
 function howMuchILoveYou(nbPetals) {
-    // your code
+    const phrases = [
+      "not at all",
+      "I love you",
+      "a little",
+      "a lot",
+      "passionately",
+      "madly"
+    ];
+  
+    return phrases[nbPetals % 6];
 }
