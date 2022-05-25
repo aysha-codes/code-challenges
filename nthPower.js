a6b958fea7136a1451d867157a0552d69eb0688a
aysha-codes <aysha.springston@gmail.com>
2022-05-25 17:51:24 -0500

    Add solution

--- nthPower.js
+++ nthPower.js
@@ -7,5 +7,6 @@
 //     array = [1, 2, 3, 4] and N = 2, then the result is 3^2 == 9;
 //     array = [1, 2, 3] and N = 3, but N is outside of the array, so the result is -1.
 function index(array, n){
-    //your code here
+    if (array.length-1-n<0) return -1
+    return Math.pow(array[n],n)
 }
