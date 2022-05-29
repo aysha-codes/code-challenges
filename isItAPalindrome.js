0df561fab96ff41eaaa07f24ab7556587435d41a
aysha-codes <aysha.springston@gmail.com>
2022-05-29 14:33:48 -0500

    Refactor solution

--- isItAPalindrome.js
+++ isItAPalindrome.js
@@ -1,5 +1,4 @@
 // Write a function that checks if a given string (case insensitive) is a palindrome.
 function isPalindrome(x) {
-    x=x.toLowerCase()
-    return x===x.split``.reverse().join``
+    return x.split("").reverse().join("").toLowerCase() === x.toLowerCase() ? true : false
 }
